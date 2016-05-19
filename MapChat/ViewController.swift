616b2fe09859f608a596b0bc71f1b2bf3ff50117
Joel Elizaga <joelizaga@hotmail.com>
2016-05-18 22:24:25 -0700

    Xcode app files added.

---
+++ MapChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapChat
+//
+//  Created by Joel Elizaga on 5/18/16.
+//  Copyright © 2016 Neward's Favs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MapChat
+
+MapChat randomly selects a user within your vicinity for you to chat with, yo!
