0bd9c056cf9072d8849b061ff2eaef3896b4719b
Joel Elizaga <joelizaga@hotmail.com>
2016-05-19 16:56:40 -0700

    Stuff.

--- MapChat/ViewController.swift
+++ MapChat/ViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // IBOutlets //////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    
+        // MAP SCENE: sliderRadiusSlider - Slider beneath the map used to adjust the radius of
+        // prospective chatters.
+        @IBOutlet weak var sliderRadiusSlider: UISlider!
+    
+        // MAP SCENE: lblRadius - Label beneath the slider indicating the currently selected
+        // radius.
+        @IBOutlet weak var lblRadius: UILabel!
+    
+        // MAP SCENE: bttnGo - Button beneath the slider. Randomly selects a person within the
+        // radius and redirects the user to a new chat with them upon being pressed.
+        @IBOutlet weak var bttnGo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
