a1677285e7a186809745453ab1c5eb5862c9d260
Joel Elizaga <joelizaga@hotmail.com>
2016-05-24 17:19:00 -0700

    Marginal progress in Chat Menu.

--- MapChat/OngoingChatTableViewCell.swift
+++ MapChat/OngoingChatTableViewCell.swift
@@ -6,10 +6,19 @@
 //  Copyright © 2016 Neward's Favs. All rights reserved.
 //
 
+// OngoingChatTableViewCell: Handles the cells of the TableView in the Chat Menu.
+
 import UIKit
 
 class OngoingChatTableViewCell: UITableViewCell {
-
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // IBOutlets //////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    
+        // lblOngoingChat: The text displayed in the cell, indicating an ongoing chat.
+        @IBOutlet weak var lblOngoingChat: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
