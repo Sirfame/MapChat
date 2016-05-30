ee5b067a430d244dc00ae614d823778a8f94b2ee
Joel Elizaga <joelizaga@hotmail.com>
2016-05-29 22:00:48 -0700

    TableViews are confusing. Will finish later.

--- MapChat/ChatTableViewCell.swift
+++ MapChat/ChatTableViewCell.swift
@@ -21,13 +21,10 @@ class ChatTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- MapChat/OngoingChatTableViewCell.swift
+++ MapChat/OngoingChatTableViewCell.swift
@@ -21,13 +21,10 @@ class OngoingChatTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
