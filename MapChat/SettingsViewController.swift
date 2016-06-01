07f6500ca521a42c409c8ae357baf782f473521b
jelizaga <jelizaga@uw.edu>
2016-06-01 10:40:04 -0700

    Reformatting Settings controller.

--- MapChat/SettingsViewController.swift
+++ MapChat/SettingsViewController.swift
@@ -16,14 +16,28 @@ class SettingsViewController : UIViewController {
     
     var usersRef = Firebase(url: "https://mapchat-2d278.firebaseio.com/users/\(Device.DeviceId)")
     
-    @IBOutlet weak var UserNameLabel: UILabel!
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // IBOutlets //////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     
-    @IBAction func ChangeProfilePic(sender: AnyObject) {
-        
+    // lblUserName - Label displaying the user's name.
+    @IBOutlet weak var lblUserName: UILabel!
+    // txtfldUserName - TextField to input the user's name.
+    @IBOutlet weak var txtfldUserName: UITextField!
+    @IBOutlet weak var UserNameLabel: UILabel!
+    // bttnChangePic - Button pressed when changing user's pic.
+    @IBOutlet weak var bttnChangePic: UIButton!
+    // bttnChangeName - Button pressed when changing user's name.
+    @IBOutlet weak var bttnChangeName: UIButton!
+    
+    // changeUserPic - Executed upon clicking bttnChangePic; changes the user's pic.
+    @IBAction func changeUserPic(sender: UIButton) {
+        NSLog("Changing user pic.")
     }
     
-    @IBAction func ChangeUserName(sender: AnyObject) {
-
+    // changeUserName - Executed upon clicking bttnChangeName; changes the user's name.
+    @IBAction func changeUserName(sender: UIButton) {
+        NSLog("Changing user name.")
     }
     
     override func didReceiveMemoryWarning() {
