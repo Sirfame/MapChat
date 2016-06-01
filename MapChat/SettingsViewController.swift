96dd7b8c0f4d5f5d0357e44091a2652c8ccf38db
chris <chris@chris.com>
2016-05-31 19:55:12 -0700

    rearranging the settings page

---
+++ MapChat/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  MapChat
+//
+//  Created by iGuest on 5/31/16.
+//  Copyright © 2016 Neward's Favs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController : UIViewController {
+    
+    @IBOutlet weak var UserNameLabel: UILabel!
+    
+    @IBAction func ChangeProfilePic(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func ChangeUserName(sender: AnyObject) {
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBOutlet weak var textField: UITextField!
+    
+    /*func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.textField.resignFirstResponder()
+    }
+    */
+    override func viewDidLoad() {
+
+    }
+}
