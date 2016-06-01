15aa62f54791033aca33a5d72df61025f0481160
Alyssa <ahudon@uw.edu>
2016-05-31 19:53:47 -0700

    getting messaging screen working with static data

--- MapChat/MessageViewController.swift
+++ MapChat/MessageViewController.swift
@@ -12,7 +12,60 @@ import JSQMessagesViewController
 import Firebase
 
 class MessageViewController : JSQMessagesViewController {
+    var messages = [JSQMessage]()
+    var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    var incomingBubbleImageView: JSQMessagesBubbleImage!
     
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
     
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource {
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            return outgoingBubbleImageView
+        } else {
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    private func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleBlueColor())
+        incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleLightGrayColor())
+    }
+    
+    func addMessage(id : String, displayName: String, text: String) {
+        let message = JSQMessage(senderId: id, displayName: displayName, text: text)
+        messages.append(message)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Chat"
+        setupBubbles()
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSizeZero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSizeZero
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        addMessage("hi", displayName: "Alyssa", text: "Hi thereee")
+        
+        addMessage(senderId, displayName: "Alyssa", text: "Hi thereee")
+        finishReceivingMessage()
+    }
+
 }
+
 
